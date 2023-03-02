922ce5e5a7acc1d7b1f784746826b268a597c363
hardikgosai2310 <hardik.gosai@einfochips.com>
2023-03-02 11:37:17 +0530

    first pytest exer
    Added login header test

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,9 +1,7 @@
 from assertpy import assert_that
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-
-# url = https://opensource-demo.orangehrmlive.com/
-# title = 'OrangeHRM'
 
 class TestLoginUI:
     def test_title(self):
@@ -14,3 +12,11 @@ class TestLoginUI:
         actual_title = driver.title
         assert actual_title == 'OrangeHRM'
         assert_that('OrangeHRM').is_equal_to(actual_title)
+
+    def test_header(self):
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.implicitly_wait(10)
+        driver.get('https://opensource-demo.orangehrmlive.com/')
+        actual_header = driver.find_element(By.XPATH, '//h5').text
+        assert_that(actual_header).is_equal_to('Login')
