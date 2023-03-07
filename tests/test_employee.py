16e1e2bde4f32e4b4e679f135eae626d0d741a52
hardikgosai2310 <hardik.gosai@einfochips.com>
2023-03-07 12:52:13 +0530

    added assignment 1

--- tests/test_employee.py
+++ tests/test_employee.py
@@ -9,21 +9,23 @@ from utilities import data_source
 
 
 class TestEmployee(WebDriverWrapper):
-    @pytest.mark.parametrize("username, password", data_source.test_valid_login_data)
-    def test_valid_login(self, username, password):
+    @pytest.mark.parametrize("username, password, first_name, middle_name, last_name, full_name, verify_f_name",
+                             data_source.test_add_employee_data)
+    def test_valid_login(self, username, password, first_name, middle_name, last_name, full_name, verify_f_name):
         self.driver.find_element(By.NAME, "username").send_keys(username)
         self.driver.find_element(By.NAME, "password").send_keys(password)
         self.driver.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
 
         self.driver.find_element(By.LINK_TEXT, 'PIM').click()
         self.driver.find_element(By.LINK_TEXT, 'Add Employee').click()
-        self.driver.find_element(By.NAME, 'firstName').send_keys('John')
-        self.driver.find_element(By.NAME, 'middleName').send_keys('J')
-        self.driver.find_element(By.NAME, 'lastName').send_keys('Week')
+        self.driver.find_element(By.NAME, 'firstName').send_keys(first_name)
+        self.driver.find_element(By.NAME, 'middleName').send_keys(middle_name)
+        self.driver.find_element(By.NAME, 'lastName').send_keys(last_name)
         self.driver.find_element(By.XPATH, '//button[normalize-space()="Save"]').click()
 
         time.sleep(3)
-        # employee_name = self.driver.find_element(By.XPATH, '//div[@class="orangehrm-edit-employee-name"]/h6').text
-        # employee_first_name = self.driver.find_element(By.NAME, 'firstName').text
-        # assert_that(employee_name).is_equal_to('John Week')
-        # assert_that(employee_first_name).is_equal_to('John')
+        employee_name = self.driver.find_element(By.XPATH,
+                                                 f'//h6[contains(normalize-space(),"{first_name}")]').text
+        employee_first_name = self.driver.find_element(By.NAME, "firstName").get_attribute("value")
+        assert_that(employee_name).is_equal_to(full_name)
+        assert_that(employee_first_name).is_equal_to(first_name)
--- utilities/data_source.py
+++ utilities/data_source.py
@@ -1,8 +1,13 @@
-test_invalid_login_data =  [
+test_invalid_login_data = [
     ("Admin1", "admin1", "Invalid credentials"),
     ("Admin2", "admin2", "Invalid credentials")
 ]
 
 test_valid_login_data = [
     ("Admin", "admin123")
+]
+
+test_add_employee_data = [
+    ("Admin", "admin123", "John", "J", "Wick", "John Wick", "John"),
+    ("Admin", "admin123", "Peter", "J", "Wick", "Peter Wick", "Peter")
 ]
